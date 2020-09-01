a1fab22ca5b5742120337e3b628108a76b7159ce
Mousumi Masapu <mousumi.masapu@gmail.com>
2020-09-01 13:47:41 +0200

    WebAPI from scratch done and tested.

---
+++ WebAPI/WebAPIScratch/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIScratch.Controllers;
+using WebAPIScratch.Models;
+
+namespace WebAPIScratch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        List<Product> products = new List<Product>()
+        {
+        new Product{
+            Id=1006368,
+            Name="Austin and BBQ AABQ Wifi Food Thermometer",
+            Description="Thermometer and med wifi for en optimal innertemparatiure",
+            Price=399
+
+
+            },
+     new Product{
+            Id=1009334,
+            Name="Anderson Elektrisk Tandare ECL 1.1",
+            Description="Elektrisk stormsaker tander helt utan gas ovh bransel",
+            Price=89
+
+
+            },
+     new Product
+     {
+     Id = 1002266,
+     Name = "Webber Non Stick Spray",
+     Description = "BBQ olijespray som motverkar att ravaror pa gallet",
+     Price = 399
+
+
+     }
+ 
+    };
+
+[HttpGet]
+
+public IEnumerable<Product> Get()
+{
+    return products;
+
+
+}
+
+        [HttpGet("{id}")]
+
+        public Product Get(int id)
+        {
+            var product = products.Find(product => product.Id == id);
+            return product;
+
+
+        }
+
+
+    }
+}
--- WebAPI/WebAPIScratch/Startup.cs
+++ WebAPI/WebAPIScratch/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,29 +13,36 @@ namespace WebAPIScratch
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration{get; }
+
+        public Startup(IConfiguration configuration)
+        { 
+            Configuration = configuration; 
+        
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endPoints =>
             {
-                app.UseDeveloperExceptionPage();
+                endPoints.MapControllers();
+
             }
 
-            app.UseRouting();
+            );
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
+
+
+
+
