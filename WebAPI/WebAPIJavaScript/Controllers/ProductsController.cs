57bf2683da2d9a5743fd9cd437e6df6a28502509
Mousumi Masapu <mousumi.masapu@gmail.com>
2020-09-02 11:37:48 +0200

    Web APl with Javascript Lab done

---
+++ WebAPI/WebAPIJavaScript/Controllers/ProductsController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIJavaScript.Models;
+
+namespace WebAPIJavaScript.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>()
+        {
+        new Product{
+            Id=1006368,
+            Name="Austin and BBQ AABQ Wifi Food Thermometer",
+            Description="Thermometer and med wifi for en optimal innertemparatiure",
+            Price=399
+
+
+            },
+     new Product{
+            Id=1009334,
+            Name="Anderson Elektrisk Tandare ECL 1.1",
+            Description="Elektrisk stormsaker tander helt utan gas ovh bransel",
+            Price=89
+
+
+            },
+     new Product
+     {
+     Id = 1002266,
+     Name = "Webber Non Stick Spray",
+     Description = "BBQ olijespray som motverkar att ravaror pa gallet",
+     Price = 399
+
+
+     }
+
+    };
+
+        [HttpGet]
+
+        public IEnumerable<Product> Get()
+        {
+            return products;
+
+
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Product> Get(int id)
+        {
+            var product = products.Find(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Product product)
+        {
+            if (products.Exists(p => p.Id == product.Id))
+            {
+                return Conflict();
+            }
+            products.Add(product);
+            return CreatedAtAction(nameof(Get), new { id = product.Id }, products);
+        }
+
+
+        [HttpDelete("{id}")]
+        public ActionResult<IEnumerable<Product>> Delete(int id)
+        {
+            var product = products.Where(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            products = products.Except(product).ToList();
+            return products;
+        }
+        [HttpPut("{id}")]
+        public ActionResult<IEnumerable<Product>> Put(int id, [FromBody] Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+            var exsistingProduct = products.Where(p => p.Id == id);
+            products = products.Except(exsistingProduct).ToList();
+            products.Add(product);
+            return products;
+        }
+
+    }
+}
--- WebAPI/WebAPIJavaScript/Startup.cs
+++ WebAPI/WebAPIJavaScript/Startup.cs
@@ -7,33 +7,31 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace WebAPIJavaScript
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            services.AddControllers();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseCors(options => options.WithOrigins("*").AllowAnyMethod());
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.UseAuthorization();
+            app.UseEndpoints(endPoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endPoints.MapControllers();
             });
         }
     }
