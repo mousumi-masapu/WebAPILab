f40e6ecf56d5ea57a57dbc3572c7939747be67e3
Mousumi Masapu <mousumi.masapu@gmail.com>
2020-09-03 11:01:07 +0200

    WebAPI with Entity Framework Lab done.

--- WebAPI/WebAPIEntityFW/Models/ApiContext.cs
+++ WebAPI/WebAPIEntityFW/Models/ApiContext.cs
@@ -19,11 +19,7 @@ namespace WebAPIEntityFW.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=WebAPIDB;Integrated Security=True");
-            }
+           
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- WebAPI/WebAPIEntityFW/Startup.cs
+++ WebAPI/WebAPIEntityFW/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebAPIEntityFW.Models;
 
 namespace WebAPIEntityFW
 {
@@ -25,6 +27,11 @@ namespace WebAPIEntityFW
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ApiContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("SqlConnection")));
+            services.AddMvc(options => options.EnableEndpointRouting = false).
+                SetCompatibilityVersion(CompatibilityVersion.Latest).AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
