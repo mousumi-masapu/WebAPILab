ab45c6f765bfc1dfe842e81e1308e529621e1f6b
Mousumi Masapu <mousumi.masapu@gmail.com>
2020-09-03 16:38:28 +0200

    Working on Blazor API Customer.razor file

---
+++ WebAPI/BlazorWithWebAPI/WebAPIBlazor/Entities/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIBlazor.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+    public DbSet<Customer> Customers { get; set; }
+  }
+}
