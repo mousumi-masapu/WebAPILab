956c3bccbbab5c505d5ac584c3f0cf245eaee493
Mousumi Masapu <mousumi.masapu@gmail.com>
2020-09-04 15:40:59 +0200

    WebAPI Blazor Lab done and tested.

--- WebAPI/BlazorWithWebAPI/Blazor/Models/CustomerCreateModel.cs
+++ WebAPI/BlazorWithWebAPI/Blazor/Models/CustomerCreateModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,15 +8,14 @@ namespace Blazor.Models
 {
     public class CustomerCreateModel
     {
-
-        public int Id { get; set; }
-
+ 
+        [Required]
         public string FirstName { get; set; }
-
+        [Required]
         public string LastName { get; set; }
-
+        [Required]
         public string Email { get; set; }
-
+        [Required]
         public string Password { get; set; }
 
 
