4b9775129f1c5f3fe1b056a0f3490a7ae393ff35
Mousumi Masapu <mousumi.masapu@gmail.com>
2020-09-07 15:00:22 +0200

    JSON API Deserialization Exercise done.

--- WebAPI/JsonAPISerialization/JsonAPISerialization/Program.cs
+++ WebAPI/JsonAPISerialization/JsonAPISerialization/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Web.Script.Serialization;
 
 namespace JsonAPISerialization
 {
@@ -13,47 +16,79 @@ namespace JsonAPISerialization
         static void Main(string[] args)
         {
 
+            string path = @"D:\MVC Cloud\json\student.json";
+            if (File.Exists(path))
+            {
+                using (StreamReader fileReader = new StreamReader(path))
+                {
+                    string jsonFileReader = fileReader.ReadToEnd();
+                    //Converts between .net types and Json types
+                    //Deserializes a JSON to .NET object and returns deserialized object from the json string.
+                    Student studentObj =JsonConvert.DeserializeObject<Student>(jsonFileReader);
+                    Console.WriteLine(studentObj.Name);
+                    Console.WriteLine(studentObj.RollNo);
+                    Console.WriteLine(studentObj.Grade);
+                    foreach (object value in studentObj.Subjects)
+                    {
+                        Console.WriteLine("Subjects:" +value);
+
+                    }
+                    Console.Read();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("File Not Found");
+                Console.Read();
+            }
+
             //Employee emp = new Employee();
             //string JSONresult = JsonConvert.SerializeObject(emp);
             //string path = @"D:\MVC Cloud\json\employee.json";
 
-            Student student = new Student()
-            {
-                RollNo=1,
-                Name="Aryan",
-                Grade="Grade One",
-                Subjects=new List<string>()
-                {
-                    "English",
-                    "Math",
-                    "Swedish"
-                }
 
-            };
-            string JSONresult = JsonConvert.SerializeObject(student);
-            string path = @"D:\MVC Cloud\json\student.json";
 
-            if (File.Exists(path)) {
-                File.Delete(path);
-                using (var tw = new StreamWriter(path, true))
-                {
-                    tw.WriteLine(JSONresult.ToString());
-                    tw.Close();
 
-                }
-            }
+            //    Student student = new Student()
+            //    {
+            //        RollNo=1,
+            //        Name="Aryan",
+            //        Grade="Grade One",
+            //        Subjects=new List<string>()
+            //        {
+            //            "English",
+            //            "Math",
+            //            "Swedish"
+            //        }
 
-            else if (!File.Exists(path)) { 
 
-                using (var tw = new StreamWriter(path, true)) { 
 
-                    tw.WriteLine(JSONresult.ToString());
-                    tw.Close();
-                }
-            }
+            //    };
+            //    string JSONresult = JsonConvert.SerializeObject(student);
+            //    string path = @"D:\MVC Cloud\json\student.json";
+
+            //    if (File.Exists(path)) {
+            //        File.Delete(path);
+            //        using (var tw = new StreamWriter(path, true))
+            //        {
+            //            tw.WriteLine(JSONresult.ToString());
+            //            tw.Close();
+
+            //        }
+            //    }
+
+            //    else if (!File.Exists(path)) { 
+
+            //        using (var tw = new StreamWriter(path, true)) { 
+
+            //            tw.WriteLine(JSONresult.ToString());
+            //            tw.Close();
+            //        }
+            //    }
 
         }
-        
-            
+
+
     }
 }
