2b388534cbc95ab36ce25d219d23158fc7e99acd
Mousumi Masapu <mousumi.masapu@gmail.com>
2020-09-07 10:49:32 +0200

    JSON API serialization lab done

---
+++ WebAPI/JsonAPISerialization/JsonAPISerialization/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonAPISerialization
+{
+    class Employee
+    {
+
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        //public int employeeId { get; set; }
+        //public string Designation { get; set; }
+
+        public string FirstName = "Mousumi";
+        public string LastName = "Masapu";
+        public int employeeId = 1234567;
+        public string Designation = "Developer";
+
+
+    }
+}
