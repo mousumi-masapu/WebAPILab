17f601a5f2435b2ceb35d41e60ea932cadb5a776
Mousumi Masapu <mousumi.masapu@gmail.com>
2020-09-07 11:02:05 +0200

    JSON API serialization Exercise 1 done

--- WebAPI/JsonAPISerialization/JsonAPISerialization/Program.cs
+++ WebAPI/JsonAPISerialization/JsonAPISerialization/Program.cs
@@ -13,10 +13,26 @@ namespace JsonAPISerialization
         static void Main(string[] args)
         {
 
-            Employee emp = new Employee();
-            string JSONresult = JsonConvert.SerializeObject(emp);
-            string path = @"D:\MVC Cloud\json\employee.json";
-            
+            //Employee emp = new Employee();
+            //string JSONresult = JsonConvert.SerializeObject(emp);
+            //string path = @"D:\MVC Cloud\json\employee.json";
+
+            Student student = new Student()
+            {
+                RollNo=1,
+                Name="Aryan",
+                Grade="Grade One",
+                Subjects=new List<string>()
+                {
+                    "English",
+                    "Math",
+                    "Swedish"
+                }
+
+            };
+            string JSONresult = JsonConvert.SerializeObject(student);
+            string path = @"D:\MVC Cloud\json\student.json";
+
             if (File.Exists(path)) {
                 File.Delete(path);
                 using (var tw = new StreamWriter(path, true))
---
+++ WebAPI/JsonAPISerialization/JsonAPISerialization/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonAPISerialization
+{
+    class Student
+    {
+
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+
+        public string Grade { get; set; }
+        public List<string> Subjects { get; set; }
+
+
+    }
+}
